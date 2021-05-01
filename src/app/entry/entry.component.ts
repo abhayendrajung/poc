6c68f63ce24b34f5df9bac6d8536f22b6fe717b4
Abhayendra <abhayendrajung61@gmail.com>
2021-05-01 09:39:28 +0530

    optimised

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog"
-import {FormService} from "../form.service"
-import {FormGroup ,Validators} from "@angular/forms"
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormService } from '../form.service';
+
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.scss']
+  styleUrls: ['./entry.component.scss'],
 })
 export class EntryComponent implements OnInit {
-
-  constructor(private dialogRef:MatDialogRef <EntryComponent>,
-    public service   :FormService) {
-     
-     }
+  constructor(
+    private dialogRef: MatDialogRef<EntryComponent>,
+    public service: FormService
+  ) {}
 
   ngOnInit(): void {
     this.service.form.reset();
-     this.service.form.patchValue({id :Math.floor(Math.random()*1000)})
+    this.service.form.patchValue({ id: Math.floor(Math.random() * 1000) });
   }
   onSubmit() {
-    console.log("data",this.service.form.value)
+    console.log('data', this.service.form.value);
     this.dialogRef.close(this.service.form.value);
   }
-
 }
